a490030d9d7e012b08297988fd1c2bfde1250a5f
ErDigvijay407 <er.digvijay407@gmail.com>
2025-11-14 13:11:43 +0530

    updated

--- server/routes/signIn.js
+++ server/routes/signIn.js
@@ -1,36 +1,24 @@
 const express = require('express')
-const bcrypt = require('bcrypt');
 
 const pool = require('../utils/dbuser')
 
 const router = express.Router()
 router.put('/signIn', (req, res) => {
     const { email, password } = req.body
-    const sql = `SELECT * FROM users WHERE email = ?`
-    pool.query(sql, [email], async (error, data) => {
+    const sql = `SELECT * FROM users WHERE email = ? and password = ?`
+    pool.query(sql, [email, password],(error, data) => {
         if (data != '') {
-            try {
-                const user = data[0]
-                const result = await bcrypt.compare(password, user.password)
-                if (result) {
-                    const userData = {
-                        id: user.id,
-                        first_name: user.first_name,
-                        last_name: user.last_name,
-                        email: user.email
-                    }
+            const user = data[0]
+                const userData = {
+                    id: user.id,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    email: user.email
+                }
                     res.send({ status: 'success', data: userData })
-                }
-                else {
-                    res.send({ status: 'error', error: 'Invalid Password' })
-                }
-            }
-            catch (error) {
-                res.send({ status: 'error', error })
-            }
         }
         else {
-            res.send({ status: 'error', error: 'Invalid email' })
+            res.send({ status: 'error', error: 'Invalid email or Password' })
         }
 
     })
