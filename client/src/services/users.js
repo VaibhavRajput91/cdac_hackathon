16e02b49a357f610359f9554484b6557f6fe53b3
Vaibhav Rajput <vaibhavrajputhk2003@gmail.com>
2025-11-14 13:26:44 +0530

    frontend

---
+++ client/src/services/users.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+export async function register(fname, lname, email, mobile, dob, password){
+    try{
+        const url = "http://localhost:4100/signUp";
+
+        const body = {
+            "first_name" : fname,
+            "last_name" : lname,
+            "email" : email,
+            "mobile" : mobile,
+            "birth" : dob,
+            "password" : password
+        }
+
+        const response = await axios.post(url, body)
+
+        return response.data
+    }
+    catch(ex){
+        console.log("Exception : " + ex)
+    }
+}
+
+export async function login(email, password){
+    try{
+        const url = "http://localhost:4100/signIn"
+
+        const body = {
+            "email" : email,
+            "password" : password
+        }
+
+        const response = await axios.get(url, body)
+
+        return response.data
+    }
+    catch(ex){
+        console.log("Exception : " + ex)
+    }
+}
